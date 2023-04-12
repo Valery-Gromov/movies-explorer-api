0288029018dcc2d4e5c66fb1a34b438bf5de3cf9
Valerii Gromov <kar.tell@yandex.ru>
2023-04-13 00:00:13 +0300

    fix movie route

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { createMovieDataValidator, deleteMovieDataValidator } = require('../utills/validation');
 
 router.get('/', getMovies);
-router.post('/me', createMovieDataValidator(), createMovie);
+router.post('/', createMovieDataValidator(), createMovie);
 router.delete('/:_id', deleteMovieDataValidator(), deleteMovie);
 
 module.exports = router;
